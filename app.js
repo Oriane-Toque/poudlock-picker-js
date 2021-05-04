b29864e871363ea3fff4ee21060996391b32b872
Oriane-Toque <oriane.toque@gmail.com>
2021-05-04 19:12:06 +0200

    check input user: done

--- app.js
+++ app.js
@@ -11,7 +11,46 @@ let app = {
     // Méthode appelée au chargement de la page
     init: function() {
      
-    }
+			// récupération du formulaire + écouteur d'évènement
+      const formChoixPo = document.querySelector('form');
+			formChoixPo.addEventListener('submit', app.handleFamilyGroup);  
+    },
+
+		handleFamilyGroup: function(evt) {
+			
+			// stoppe l'envoi des données
+			evt.preventDefault();
+			
+			// appelle d'une fonction qui check si la saisie est correcte
+			if (app.checkInputUser() === true) {
+				
+				// appelle d'une fonction qui check les données pour correspondre à une famille
+				app.convertToFamily();
+			}
+			
+			
+
+		},
+
+		convertToFamily: function() {
+			// =========
+		},
+
+		checkInputUser: function() {
+
+			// récupération input + valeur
+			const inputUser = document.querySelector('.name');
+			const inputUserValue = inputUser.value;
+			// clear input
+			inputUser.value = '';
+			
+			if(inputUserValue !== undefined && isNaN(inputUserValue) && inputUserValue.length > 2) {
+				console.log(true);
+			}
+			else {
+				console.log(false);
+			}
+		}
 }
 
 // Quand la page est entièrement chargée, on exécute la méthode init située dans l'object app.
